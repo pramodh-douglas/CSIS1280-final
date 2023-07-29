23d9113980cf7ef0a0094d40dada4a5cb905110c
Nadeera <nadeeraderaniyagala@gmail.com>
2023-07-28 18:41:51 -0700

    About us page is done

---
+++ js/about.js
@@ -0,0 +1,27 @@
+const AboutUs = (function () {
+  const _showMore = function () {
+    $('#readMore').fadeIn(1000);
+    const readMoreButton = $('#readMoreBtn');
+    readMoreButton.unbind('click').bind('click', _showLess);
+    readMoreButton.text('Read Less');
+  };
+
+  const _showLess = function () {
+    $('#readMore').fadeOut(500);
+    const readMoreButton = $('#readMoreBtn');
+    readMoreButton.unbind('click').bind('click', _showMore);
+    readMoreButton.text('Read More');
+  }
+
+  const _init = function () {
+    const readMoreButton = $('#readMoreBtn');
+    readMoreButton.on('click', _showMore);
+  };
+
+  return {
+    showMore: _showMore,
+    init: _init,
+  };
+})();
+
+AboutUs.init();
